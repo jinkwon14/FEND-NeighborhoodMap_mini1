38357f2da651dd3d60d1627b1f975ec16835da48
JinK <jinkwon0425@gmail.com>
2016-11-15 20:26:47 -0500

    add background image using google streetview api

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 
 function loadData() {
-
     var $body = $('body');
     var $wikiElem = $('#wikipedia-links');
     var $nytHeaderElem = $('#nytimes-header');
@@ -11,11 +10,21 @@ function loadData() {
     $wikiElem.text("");
     $nytElem.text("");
 
-    // load streetview
+    // !! This is how to call the data from form
+    var streetStr = $('#street').val();
+    var cityStr = $('#city').val();
+    var address = streetStr + ', ' + cityStr;
 
+    $greeting.text('So you want to live at ' + address + '?');
+
+    var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
     // YOUR CODE GOES HERE!
+    $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
     return false;
-};
+}
 
 $('#form-container').submit(loadData);
+
+// You'll also need to use jQuery's append method to add an <img> to the page. Make sure it has a class of bgimg. For example:
+// Notice how the new <img> HTML element is just a string passed into .append().
