ea6d2dac1e859ad982cd712ef8f94710ec38c51e
JinK <jinkwon0425@gmail.com>
2016-11-15 21:27:03 -0500

    integrate New York Times API

--- js/script.js
+++ js/script.js
@@ -18,13 +18,27 @@ function loadData() {
     $greeting.text('So you want to live at ' + address + '?');
 
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location=' + address + '';
-    // YOUR CODE GOES HERE!
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
+    var newyorktimesURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q=' + cityStr + '&sort=newest&api-key=d3b3ebf311294423b96fa5571680868e';
+    $.getJSON(newyorktimesURL, function(data) {
+      $nytHeaderElem.text('New York Times Ariticle About ' + cityStr);
+
+      var articles = data.response.docs;
+      var articleSize = articles.length;
+      var i;
+      var article;
+      for (i = 0; i < articleSize; i++) {
+        article = articles[i];
+        $nytElem.append('<li class=article>' +
+          '<a href="' + article.web_url+'">' + article.headline.main +
+          '</a>' +
+        '<p>' + article.snippet + '</p>' +
+        '</li>');
+      }
+      console.log(data.response.docs);
+    });
     return false;
 }
 
 $('#form-container').submit(loadData);
-
-// You'll also need to use jQuery's append method to add an <img> to the page. Make sure it has a class of bgimg. For example:
-// Notice how the new <img> HTML element is just a string passed into .append().
